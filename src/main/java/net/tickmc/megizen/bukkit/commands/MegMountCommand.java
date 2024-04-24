2bf4777b425785a55baffa8a8128ee3ff6ce892e
hyper <honkhonk4456@gmail.com>
2024-04-24 16:03:26 -0400

    .

--- src/main/java/net/tickmc/megizen/bukkit/commands/MegMountCommand.java
+++ src/main/java/net/tickmc/megizen/bukkit/commands/MegMountCommand.java
@@ -8,20 +8,28 @@ import com.denizenscript.denizencore.scripts.commands.generator.ArgDefaultText;
 import com.denizenscript.denizencore.scripts.commands.generator.ArgName;
 import com.denizenscript.denizencore.scripts.commands.generator.ArgPrefixed;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
+import com.ticxo.modelengine.api.ModelEngineAPI;
 import com.ticxo.modelengine.api.model.ActiveModel;
+import com.ticxo.modelengine.api.model.ModeledEntity;
+import com.ticxo.modelengine.api.model.bone.behavior.GlobalBehaviorData;
+import com.ticxo.modelengine.api.model.bone.manager.BehaviorManager;
+import com.ticxo.modelengine.api.model.bone.manager.MountData;
+import com.ticxo.modelengine.api.model.bone.manager.MountManager;
 import com.ticxo.modelengine.api.mount.controller.MountControllerTypes;
 import net.tickmc.megizen.bukkit.objects.MegActiveModelTag;
+import net.tickmc.megizen.bukkit.objects.MegBoneTag;
+import net.tickmc.megizen.bukkit.objects.MegModeledEntityTag;
 
 public class MegMountCommand extends AbstractCommand {
     public MegMountCommand() {
         setName("megmount");
-        setSyntax("megmount [entity:<entity>] [active_model:<active_model>] [type:driver/passenger]");
+        setSyntax("megmount [entity:<entity>] [model:<active_model>] (driver) (passenger)");
         autoCompile();
     }
 
     // <--[command]
     // @Name MegState
-    // @Syntax megmount [entity:<entity>] [active_model:<active_model>] [type:driver/passenger]
+    // @Syntax megmount [type:driver/passenger] [entity:<entity>] [model:<active_model>]
     // @Required 3
     // @Short Mounts the given entity on the given modeled entity, either as a passenger or the driver.
     // @Group Megizen
@@ -33,22 +41,25 @@ public class MegMountCommand extends AbstractCommand {
 
     public static void autoExecute(ScriptEntry scriptEntry,
                                    @ArgName("entity") @ArgPrefixed EntityTag entity,
-                                   @ArgName("active_model") @ArgPrefixed MegActiveModelTag model,
-                                   @ArgName("type") @ArgPrefixed String string) {
-        ActiveModel activeModel = model.getActiveModel();
+                                   @ArgName("model") @ArgPrefixed MegActiveModelTag model,
+                                   @ArgName("driver") @ArgDefaultNull boolean driver,
+                                   @ArgName("passenger") @ArgDefaultNull boolean passenger,
+                                   @ArgName("mount_interactable") boolean interactable,
+                                   @ArgName("mount_damageable") boolean damageable) {
         if (entity == null) {
             Debug.echoError("The 'entity' argument is required to mount an entity.");
             return;
         }
-        if (activeModel == null) {
+        if (model == null) {
             Debug.echoError("The 'model' argument is required to mount an entity.");
             return;
         }
-        if (!string.equalsIgnoreCase("driver") && !string.equalsIgnoreCase("passenger")) {
-            Debug.echoError("The 'driver/passenger' argument is required to mount an entity.");
+        if (!driver && !passenger) {
+            Debug.echoError("A type of 'driver' or 'passenger' must be specified.");
             return;
         }
-        if (string.equalsIgnoreCase("driver")) {
+        ActiveModel activeModel = model.getActiveModel();
+        if (driver) {
             activeModel.getMountManager().ifPresent(mountManager -> {
                 if (mountManager.getDriver() != null) {
                     mountManager.dismountDriver();
@@ -56,11 +67,10 @@ public class MegMountCommand extends AbstractCommand {
                 mountManager.mountDriver(entity.entity, MountControllerTypes.WALKING);
             });
         }
-        else if (string.equalsIgnoreCase("passenger")){
+        else {
             activeModel.getMountManager().ifPresent(mountManager -> {
-                mountManager.mountPassenger("seat", entity.entity, MountControllerTypes.WALKING);
+                mountManager.mountPassenger(mountManager.getPassengerSeatMap().forEach( ).entity, MountControllerTypes.WALKING);
             });
         }
-
     }
 }
